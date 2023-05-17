2dc641680ea59596613cf7e59919e0c82d7742e3
dbaron5 <131029961+dbaron5@users.noreply.github.com>
2023-05-17 10:15:29 -0400

    update

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const app = express();
 app.use(express.json());
 
 // Create an empty array to store restaurant data
-const restaurants = [];
+let restaurants = [
+  { name: "Uchi", style: "Asian", price: "$$$$" },
+  //  { name: "Katz's Deli", style: "Delicatessen", price: "$$" },
+];
 
 // Routes go here
+app.get("/get_restaurant_name", (req, res) => {
+  res.send(restaurants);
+});
+
+app.post("");
 
 // Start the server
 app.listen(3000, () => {
